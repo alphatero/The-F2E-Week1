19111e41905a7c5d65c96f0618d1d9545f54e430
alpha <alphatero@icloud.com>
2023-01-06 16:58:26 +0800

    feat: 🎸 question page

--- src/components/contexts/RefsProvider.tsx
+++ src/components/contexts/RefsProvider.tsx
@@ -24,37 +24,6 @@ const RefContext = createContext<RefContextTypes>({
   joinButtonRef: { current: null },
 });
 
-// function animated(element: HTMLElement) {
-//   let x = 0;
-
-//   //依照條件設定x初始值
-//   if (element.classList.contains('from-left')) {
-//     x = -100;
-//   } else if (element.classList.contains('from-right')) {
-//     x = 100;
-//   }
-
-//   //設定元素初始值
-//   element.style.transform = `translate(${x}px, 0px)`;
-//   gsap.fromTo(
-//     element,
-//     { x: x, y: 0, opacity: 0, visibility: 'hidden' },
-//     {
-//       duration: 1,
-//       delay: 0.2,
-//       x: 0,
-//       y: 0,
-//       visibility: 'visible',
-//       opacity: '1',
-//       ease: 'expo',
-//       overwrite: 'auto',
-//     }
-//   );
-// }
-// function hide(element: HTMLElement) {
-//   gsap.set(element, { opacity: 0 });
-// }
-
 export const RefProvider = ({ children }: PropsWithChildren) => {
   const readyRef = useRef(null);
   const bottomRef = useRef(null);
@@ -94,7 +63,7 @@ export const RefProvider = ({ children }: PropsWithChildren) => {
     ScrollTrigger.create({
       trigger: joinEl,
       scrub: true,
-      // markers: true,
+
       start: 'top center',
       end: 'bottom center',
       animation: gsap.fromTo(bottomEl, { scale: 0.5, duration: 2 }, { scale: 1 }),
@@ -114,7 +83,7 @@ export const RefProvider = ({ children }: PropsWithChildren) => {
     ScrollTrigger.create({
       trigger: joinEl,
       scrub: true,
-      // markers: true,
+
       start: 'top center',
       end: 'bottom center',
       animation: gsap.fromTo(joinButtonEl, { opacity: 1 }, { opacity: 0, visibility: 'hidden' }),
